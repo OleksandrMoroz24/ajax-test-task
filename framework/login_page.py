cf29446dc2ed0a085df542759dd67d19f561617b
Oleksandr Moroz <oleksandrmoroz2003@gmail.com>
2024-01-10 01:10:47 +0200

    create page and login page classes

--- framework/login_page.py
+++ framework/login_page.py
@@ -2,4 +2,34 @@ from .page import Page
 
 
 class LoginPage(Page):
-    pass
+
+    def enter_email(self, locator: tuple, email: str) -> None:
+        """ Enter email in email field """
+        self.enter_text(locator, email)
+
+    def enter_password(self, locator: tuple, password: str) -> None:
+        """ Enter password in password field """
+        self.enter_text(locator, password)
+
+    def click_login_button(self, locator: tuple) -> None:
+        """ Clic on first login button """
+        self.click_element(locator)
+
+    def click_second_login_button(self, locator: tuple) -> None:
+        """ Clic on second login button """
+        self.click_element(locator)
+
+    def login(
+            self,
+            first_login_locator: tuple,
+            second_login_locator: tuple,
+            email_locator: tuple,
+            password_locator: tuple,
+            email: str,
+            password: str
+    ) -> None:
+        """ General method for login """
+        self.click_login_button(first_login_locator)
+        self.enter_email(email_locator, email)
+        self.enter_password(password_locator, password)
+        self.click_second_login_button(second_login_locator)
--- framework/page.py
+++ framework/page.py
@@ -1,10 +1,37 @@
-class Page:
+from appium.webdriver import WebElement
 
+
+class Page:
     def __init__(self, driver):
         self.driver = driver
 
-    def find_element(self):
-        raise NotImplementedError
+    """
+    A locator is a tuple that specifies how to find a certain element.
 
-    def click_element(self):
-        raise NotImplementedError
+    The locator tuple consists of two parts:
+    1. 'by': The method to use for locating the element (e.g., MobileBy.XPATH).
+    2. 'element': The specific identifier for the element (e.g., XPath, CSS selector).
+
+    Example:
+        The following locator finds an element by its XPath:
+
+        (
+            MobileBy.XPATH,
+            "//android.widget.EditText"
+            "[@resource-id='com.ajaxsystems:id/authLoginEmail']"
+        )
+    """
+    def find_element(self, locator: tuple) -> WebElement:
+        """ Find an element by a given locator """
+        return self.driver.find_element(locator)
+
+    def click_element(self, locator: tuple) -> None:
+        """ Click on the element at the specified locator """
+        element = self.find_element(locator)
+        element.click()
+
+    def enter_text(self, locator, text) -> None:
+        """ Enter text in the input field at the specified locator """
+        element = self.find_element(locator)
+        element.clear()
+        element.send_keys(text)
