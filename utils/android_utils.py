f0d86f428133d51004a987495970e65ba33eebfb
Oleksandr Moroz <oleksandrmoroz2003@gmail.com>
2024-01-10 01:12:04 +0200

    simplified desired_capabilities

--- utils/android_utils.py
+++ utils/android_utils.py
@@ -1,15 +1,8 @@
 def android_get_desired_capabilities():
     return {
-        'autoGrantPermissions': True,
-        'automationName': 'uiautomator2',
-        'newCommandTimeout': 500,
-        'noSign': True,
         'platformName': 'Android',
-        'platformVersion': '10',
-        'resetKeyboard': True,
-        'systemPort': 8301,
-        'takesScreenshot': True,
-        'udid': '11bd127d',
+        'automationName': 'uiautomator2',
+        'deviceName': 'emulator-5554',
         'appPackage': 'com.ajaxsystems',
         'appActivity': 'com.ajaxsystems.ui.activity.LauncherActivity'
-}
+    }
